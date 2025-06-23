62f95e2272ab94887481274da4684d1173fd73c8
KamilCzajczyk <61652376+KamilCzajczyk@users.noreply.github.com>
2025-06-23 23:38:29 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+WINDOW_CAPTION = "Environment Mapping - Cube Map"
+FPS_LIMIT = 60
+
+CAMERA_SPEED = 0.3
+MOUSE_SENSITIVITY = 0.5
+CAMERA_PITCH_LIMIT = 89.0
+
+BACKGROUND_COLOR = (0.1, 0.1, 0.2, 1.0)
+NEAR_CLIP = 0.1
+FAR_CLIP = 1000.0
+FOV = 45
+
+CUBEMAP_FACES = [
+    "px.jpg",  
+    "nx.jpg",  
+    "py.jpg",  
+    "ny.jpg",  
+    "pz.jpg",  
+    "nz.jpg"   
+]
+
+CUBEMAP_SETS = [
+    "bridge", 
+    "yokohama2",
+    "yokohama3",
+    "bridge2",
+    "yokohama4",
+]
